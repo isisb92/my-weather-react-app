71a0e1b5ab0da12717eba8d35e1d5d4d20c4f27d
Isis <135667009+isisb92@users.noreply.github.com>
2023-11-12 14:05:01 -0600

    React weather app 1

---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  function showTemp(response) {
+    alert(`The temperature in ${props.city} is ${response.data.main.temp}°C`);
+  }
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=88724523008dc9e1be18f6eb6a959b67&units=metric`;
+  axios.get(apiURL).then(showTemp);
+}
